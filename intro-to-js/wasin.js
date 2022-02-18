ffc8486298a9144076f612be0f2c7d2eaaae3c2c
Shin <now.nsw@gmail.com>
2022-02-18 13:42:01 +0700

    added calculate age function & input and output  name/province function

--- intro-to-js/wasin.js
+++ intro-to-js/wasin.js
@@ -9,3 +9,27 @@ function helloWorld() {
   const h1message = document.getElementById("h1-message").innerHTML;
   alert(h1message + " 'มันไม่ใช่บัค มันคือฟีเจอร์'");
 }
+
+function calAge() {
+  const age = prompt("What Year You were Born", "1990");
+  const d = new Date();
+  let year = d.getFullYear();
+  alert(`Congratulation you live in this country ${year - age} years.`);
+}
+
+//
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+function loadProfile() {
+  const profileName = prompt("Please enter your name", "Bond, James Bond");
+  if (profileName != null) {
+    document.getElementById("my-name").innerHTML =
+      capitalizeFirstLetter(profileName);
+  }
+  const province = prompt("What is your state/province", "Bangkok");
+  if (province != null) {
+    document.getElementById("my-province").innerHTML =
+      capitalizeFirstLetter(province);
+  }
+}
